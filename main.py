9531ed99c676161aa878368dabc46b562c899dd5
Tristan-G-05 <Tristan.Garrison.C05@gmail.com>
2025-07-18 23:05:38 -0400

    counts letters in alpha order

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
+
+from stats import get_num_words
+
+from stats import get_num_letters
+
 def main():
     book_text = get_book_text('books/frankenstein.txt')
-    print(book_text)
+    count = f"{get_num_words(book_text)} words found in the document"
+    letter_count = get_num_letters(book_text)
+    print(count)
+    for key in sorted(letter_count.keys()):
+        print(f"'{key}': {letter_count[key]}")
 
 main()
 # This is a simple script to read and print the contents of a book file.
---
+++ stats.py
@@ -0,0 +1,9 @@
+def get_num_words(book_text):
+    return len(book_text.split())
+
+def get_num_letters(book_text):
+    lower_case_text = book_text.lower()
+    letter_dict = {}
+    for letter in lower_case_text:
+        letter_dict[letter] = letter_dict.get(letter, 0) + 1
+    return letter_dict
